06999e6dde05ff1e0689c40df3668fb01b10c29a
Beñat <bernardeirop@gmail.com>
2025-07-10 13:44:58 +0200

    [#] - Añadida la configuración, archivo principal (index.ts) y .gitignore

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import Fastify from 'fastify';
+import swagger from '@fastify/swagger';
+import swaggerUI from '@fastify/swagger-ui';
+
+import top10 from './endpoints/top10';
+import users from './endpoints/user';
+import enriched from './endpoints/enriched';
+
+const fastify = Fastify({ logger: true });
+
+fastify.register(swagger, {
+  swagger: {
+    info: {
+      title: 'Chess-Analytics',
+      version: '1.0.0',
+    },
+  },
+});
+fastify.register(swaggerUI, {
+  routePrefix: '/docs',
+  uiConfig: {
+    docExpansion: 'full',
+    deepLinking: false
+  },
+  staticCSP: true,
+  transformSpecification: (swaggerObject, request, reply) => {
+    return swaggerObject;
+  },
+  transformSpecificationClone: true
+});
+fastify.register(top10);
+fastify.register(users);
+fastify.register(enriched);
+
+const PORT = 3000;
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: PORT, host: '0.0.0.0' });
+    console.log(`Server is running on http://localhost:${PORT}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
