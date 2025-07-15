bf09b256a250b6f5eaa18b477b34aa0721e46483
Beñat <bernardeirop@gmail.com>
2025-07-15 11:43:59 +0200

    [#] - Añadidos los test E2E de enriched

--- test/endpoints/E2E/enriched.e2e.spec.ts
+++ test/endpoints/E2E/enriched.e2e.spec.ts
@@ -1,7 +1,8 @@
 import app from '../../../src/index';
 import supertest from 'supertest';
 import nock from 'nock';
-import mockLichessData from '../../mocks/enrichedUserPerformance.json'; 
+import mockLichessUserData from '../../mocks/user.json'; 
+import mockLichessEnrichedData from '../../mocks/enrichedUserPerformance.json'; 
 
 describe('Top10 E2E tests', () => { 
   let request: any;
@@ -23,7 +24,43 @@ describe('Top10 E2E tests', () => {
     nock.restore();
   });
 
-  it('should return 200 with the enriched data if the external API succeeds', async () => {
+  it('Returns 200 with the enriched data if the external API succeeds', async () => {
+    lichessApiScope.get(`/api/user/${userId}`).reply(200, mockLichessUserData);
+    lichessApiScope.get(`/api/user/${userId}/perf/${mode}`).reply(200, mockLichessEnrichedData);
+
+    const response = await request.get(`/chess/enriched?id=${userId}&mode=${mode}`);
     
+    expect(response.statusCode).toBe(200);
+
+    const body = response.body;
+    expect(body.id).toBe(userId);
+    expect(body.username).toBe('thibault');
+    expect(body.profile.bio).toBe('I turn coffee into bugs.');
+    expect(body.playTime.total).toBe(6408249);
+    expect(body.rank).toBe(null);
+    expect(body.resultStreak.wins.current).toBe(1);
+    expect(body.resultStreak.wins.max).toBe(11);
+    expect(body.resultStreak.losses.current).toBe(0);
+    expect(body.resultStreak.losses.max).toBe(19);
+
+    expect(nock.isDone()).toBe(true);
+  });
+
+  it('Returns 500 if the external Lichess API fails at first request', async () => {
+    lichessApiScope.get(`/api/user/${userId}`).reply(500, { error: 'Server Error' });
+    lichessApiScope.get(`/api/user/${userId}/perf/${mode}`).reply(200, mockLichessEnrichedData);
+    
+    const response = await request.get(`/chess/enriched?id=${userId}&mode=${mode}`);
+    
+  });
+
+  it('Returns 500 if the external Lichess API fails at second request', async () => {
+    lichessApiScope.get(`/api/user/${userId}`).reply(200, mockLichessUserData);
+    lichessApiScope.get(`/api/user/${userId}/perf/${mode}`).reply(500, { error: 'Server Error' });
+
+    const response = await request.get(`/chess/enriched?id=${userId}&mode=${mode}`);
+    
+    expect(response.statusCode).toBe(500);
+    expect(response.body).toEqual({ error: 'Internal server error.' });
   });
 });
